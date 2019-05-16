16ac7ea842d0887c6ceb2d553064c5f8c750e21e
Tiago Vieira <tiagomiguelvieira19@gmail.com>
2019-05-16 14:59:56 +0100

    ultimas modificações na aplicação

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user
       sign_in(user)
-      redirect_to user_path(user)
+      redirect_to contacts_path
     else
       flash.now[:danger] = 'Email ou senha invalidos'
       render 'new'
